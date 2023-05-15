47f72e861272616216d97ae2cb1c8a225027014b
Renan Miranda <31661482+Renanmsampaio@users.noreply.github.com>
2023-05-15 07:02:42 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,6 +19,6 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  tenant_id       = "a677c06b-f6e8-43f8-b1ad-f9c2b9542d42"
-  subscription_id = "75005f66-ab95-41f1-9463-0b7e2dee4ac3"
+  tenant_id       = ""
+  subscription_id = ""
 }
